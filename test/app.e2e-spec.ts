6cf719fd985344b1c8e86d8524936ba0506dc348
dante01yoon <bunggl@naver.com>
2021-06-14 19:55:18 +0900

    Update: e2e 테스트 추가

--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -15,10 +15,38 @@ describe('AppController (e2e)', () => {
     await app.init();
   });
 
-  it('/ (GET)', () => {
-    return request(app.getHttpServer())
-      .get('/')
-      .expect(200)
-      .expect('Hello World!');
+  describe('/', () => {
+    it('/ (GET)', () => {
+      return request(app.getHttpServer())
+        .get('/')
+        .expect(200)
+        .expect('welcome to my movie api');
+    });
+  });
+
+  describe('/movies', () => {
+    it('GET / ', () => {
+      return request(app.getHttpServer())
+        .get('/movies/')
+        .expect(200)
+        .expect([]);
+    });
+
+    it('POST', () => {
+      return request(app.getHttpServer())
+        .post('/movies')
+        .send({
+          title: 'Tenet',
+          year: 2020,
+          genres: ['action', 'thriller'],
+        })
+        .expect(201);
+    });
+
+    it('DELETE', () => {
+      return request(app.getHttpServer())
+        .delete(`/movies/1`)
+        .expect(404);
+    });
   });
 });
