3615ed7fdcce06f545b815712d51705320e42f15
danteYoon <dante@watcha.com>
2021-06-14 16:50:01 +0900

    Update: 2.6 Modules and Dependency Injection

--- src/movies/movies.service.ts
+++ src/movies/movies.service.ts
@@ -1,4 +1,6 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { CreateMovieDTO } from './dto/create-movie.dto';
+import { UpdateMovieDTO } from './dto/update-movie.dto';
 import { Movie } from './entities/movie.entity';
 
 @Injectable()
@@ -9,20 +11,31 @@ export class MoviesService {
     return this.movies;
   }
 
-  getOne(id: string): Movie {
-    return this.movies.find((movie) => movie.id === +id);
+  getOne(id: number) {
+    const movie = this.movies.find((movie) => movie.id === +id);
+    if (!movie) {
+      throw new NotFoundException(`Movie with ID ${id} not found.`);
+    }
+    return movie;
   }
 
-  deleteOne(id: string): boolean {
-    this.movies.filter((movie) => movie.id !== +id);
+  deleteOne(id: number): boolean {
+    this.getOne(id);
+    this.movies = this.movies.filter((movie) => movie.id !== +id);
     return true;
   }
 
-  create(movieData: Movie) {
+  create(movieData: CreateMovieDTO) {
     this.movies.push({
       id: this.movies.length + 1,
       ...movieData,
     });
     return this.movies;
+  }
+
+  update(movieId: number, updateData: UpdateMovieDTO) {
+    const movie = this.getOne(movieId);
+    this.deleteOne(movieId);
+    this.movies.push({ ...movie, ...updateData });
   }
 }
