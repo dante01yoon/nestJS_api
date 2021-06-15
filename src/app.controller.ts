f249d6e3c6a82c6cbf6b99b4f5758e244e0fac9b
dante01yoon <bunggl@naver.com>
2021-06-16 08:28:28 +0900

    Update: redirect, httpCode 실습

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,9 +1,29 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Header, HttpCode, Post, Redirect, Query } from '@nestjs/common';
 
 @Controller()
 export class AppController {
   @Get()
   home() {
     return `welcome to my movie api`;
+  }
+
+  @HttpCode(204)
+  @Header("Cookie", '{ "something": "something" }')
+  @Post("/session")
+  postHome() {
+    return `welcome to home`;
+  }
+
+  @Get("/docs")
+  @Redirect('https//docs.nestjs.com', 302)
+  getDocs(@Query('version') version) {
+    if (version !== 5) {
+      return (
+        {
+          url: "https://naver.com",
+          statusCode: 303
+        }
+      )
+    }
   }
 }
